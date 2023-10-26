81a4f1199ac8c6ba10d137b668c4ad8676320012
Al Wirtes <wirtes@gmail.com>
2023-10-26 06:43:26 -0600

    Posts to Mastodon

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import requests
 import json
 import random
 import sqlite3
+from mastodon import Mastodon
 from pprint import pprint
+
+def get_config():
+    try:
+        with open('./config/config.json', 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print("Config file not found.")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def pick_language():
@@ -65,8 +78,38 @@ def get_a_line():
     return random_line[0]
 
 
+def post_to_mastodon(message_obj, config):
+    message_text = "I'm just testing a bot thing.\n\n" + message_obj["final"] + "\n#SemanticDistorter"
+
+    # Initialize the Mastodon API client
+    mastodon = Mastodon(
+        access_token = config["api_key"],
+        api_base_url = config["base_url"]
+    )
+
+    # Post a status update
+    initial_status_id = mastodon.status_post(message_text)
+    # Get the ID of the original post
+    #initial_status_id = mastodon.status_post(initial_status)
+    number_of_translations = len(message_obj) - 2
+
+    reply_text = "The original text:\n\n" + message_obj["original"] + "\n\nwas translated " + str(number_of_translations) + " times."
+
+    mastodon.status_post(reply_text, in_reply_to_id = initial_status_id)
+
+    print("Toots posted successfully!")
+    return
+
+
 if __name__ == '__main__':
+    # Load up the configuration & secrets
+    config = get_config()
+    # Initialize the Message Object
+    message_obj = {}
+    # Get a line to translate from the database
     original_text_to_translate = get_a_line()
+    # Push the original line into the message object
+    message_obj["original"] = original_text_to_translate
     print ("Original: " + original_text_to_translate)
     # Pick a number of times to translate the statement
     cycles = random.randint(5, 10)
@@ -79,13 +122,18 @@ if __name__ == '__main__':
         if translate_to_language != translate_from_language:
             translated_text = translate(text_to_translate, translate_from_language, translate_to_language)
             print("Translated from " + translate_from_language + " to " + translate_to_language + ": ", translated_text)
+            message_obj[translate_to_language + "-" + str(current_cycle)] = translated_text
             translate_from_language = translate_to_language
             text_to_translate = translated_text
         current_cycle += 1
 
     # Translate back to English
     translated_text = translate(translated_text, translate_from_language, "en")
+    message_obj["final"] = translated_text
     print("Final: ", translated_text)
+    pprint(message_obj)
+    post_to_mastodon(message_obj, config)
+
 
 
 
